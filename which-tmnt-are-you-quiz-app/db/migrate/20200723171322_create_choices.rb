0aa6ec4a2a1fc2375e7db8f6a052d3710e8d54a9
Liam Tinney <ltinney@powerhrg.com>
2020-07-23 13:14:13 -0400

    Generate Models

---
+++ which-tmnt-are-you-quiz-app/db/migrate/20200723171322_create_choices.rb
@@ -0,0 +1,10 @@
+class CreateChoices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :choices do |t|
+      t.string :text
+      t.integer :question_id
+
+      t.timestamps
+    end
+  end
+end
