2ac228a24fb540e23fa891e3b5f9d1561b90a4d4
Liam Tinney <ltinney@powerhrg.com>
2020-07-24 11:11:03 -0400

    Update Seeds

--- which-tmnt-are-you-quiz-app/db/seeds.rb
+++ which-tmnt-are-you-quiz-app/db/seeds.rb
@@ -1,7 +1,145 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Question.create(content: "What is your favorite color?", choice1: "Blue", choice2: "Purple", choice3: "Red", choice4: "Orange", choice5: "Green")
+
+# [
+#   {
+#     "question": "What is your favorite color?",
+#     "choice1": "Blue",
+#     "choice2": "Purple",
+#     "choice3": "Red",
+#     "choice4": "Orange",
+#     "choice5": "Green"
+#   },
+
+Question.create(content: "Which hobby is your favorite?", choice1: "Meditating", choice2: "Reading", choice3: "Working Out", choice4: "Partying", choice5: "Watching TV")
+
+  # {
+  #   "question": "Which hobby is your favorite?",
+  #   "choice1": "Meditating",
+  #   "choice2": "Reading",
+  #   "choice3": "Working Out",
+  #   "choice4": "Partying",
+  #   "choice5": "Watching TV"
+  # },
+
+  Question.create(content: "Which type of Pizza is your favorite?", choice1: "Pepperoni & Green Peppers", choice2: "Truffle Pizza (Mixed Mushrooms, Vegan Mozzarella, Vegan Fontina, White Truffle Oil)", choice3: "Supreme (Olives, Green Pepper, Sausage, Pepperoni, Garlic)", choice4: "Pineapple & Ham", choice5: "Exxtra Cheese")
+
+
+  # {
+  #   "question": "Which type of Pizza is your favorite?",
+  #   "choice1": "Pepperoni & Green Peppers",
+  #   "choice2": "Truffle Pizza (Mixed Mushrooms, Vegan Mozzarella, Vegan Fontina, White Truffle Oil)",
+  #   "choice3": "Supreme (Olives, Green Pepper, Sausage, Pepperoni, Garlic)",
+  #   "choice4": "Pineapple & Ham",
+  #   "choice5": "Exxtra Cheese"
+  # },
+
+  Question.create(content: "Which type of Pet are you most likely to choose?", choice1: "Bird", choice2: "Salamander/Newt", choice3: "I don't want pets", choice4: "Ferret", choice5: "Turtle")
+
+
+  # {
+  #   "question": "Which type of Pet are you most likely to choose?",
+  #   "choice1": "Bird",
+  #   "choice2": "Salamander/Newt",
+  #   "choice3": "I don't want pets",
+  #   "choice4": "Ferret",
+  #   "choice5": "Turtle"
+  # },
+
+  Question.create(content: "What is your favorite Batman Villan?", choice1: "Harvey Dent ('Two Face')", choice2: "Edward Nigma ('Riddler')", choice3: "Bane", choice4: "Joker", choice5: "The Penguin")
+
+  # {
+  #   "question": "What is your favorite Batman Villan?",
+  #   "choice1": "Harvey Dent ('Two Face')",
+  #   "choice2": "Edward Nigma ('Riddler')",
+  #   "choice3": "Bane",
+  #   "choice4": "Joker",
+  #   "choice5": "The Penguin"
+  # },
+
+  Question.create(content: "Which subject in High School was your favorite?", choice1: "Foreign Cultures", choice2: "MetaPhysics", choice3: "Phys Ed", choice4: "Drama", choice5: "Philosophy")
+
+  # {
+  #   "question": "Which subject in High School was your favorite?",
+  #   "choice1": "Foreign Cultures",
+  #   "choice2": "MetaPhysics",
+  #   "choice3": "Phys Ed",
+  #   "choice4": "Drama",
+  #   "choice5": "Philosophy"
+  # },
+
+  Question.create(content: "Which is your favorite artist?", choice1: "Leonardo Di Vinci", choice2: "Donatello", choice3: "Raphael", choice4: "Michaelangelo", choice5: "Claude Monet")
+
+  # {
+  #   "question": "Which is your favorite artist?",
+  #   "choice1": "Leonardo Di Vinci",
+  #   "choice2": "Donatello",
+  #   "choice3": "Raphael",
+  #   "choice4": "Michaelangelo",
+  #   "choice5": "Claude Monet"
+  # },
+
+  Question.create(content: "Which TMNT character is most likely to be your sidekick?", choice1: "Splinter", choice2: "Casey Jones", choice3: "Chino (Pizza Delivery Guy)", choice4: "April O'Niel", choice5: "TGRI Scientist")
+
+  # {
+  #   "question": "Which TMNT character is most likely to be your sidekick?",
+  #   "choice1": "Splinter",
+  #   "choice2": "Casey Jones",
+  #   "choice3": "Chino",
+  #   "choice4": "April O'Niel",
+  #   "choice5": "TGRI Scientist"
+  # },
+
+  Question.create(content: "You are approached by an enemy, how do you react?", choice1: "Study your opponent & their movements, concentrate on them.", choice2: "Quickly strategize how to use your environment around you.", choice3: "Welcome an all out brawl.", choice4: "Try to charm your enemy to lower tensions.", choice5: "Run away.")
+
+  # {
+  #   "question": "You are approached by an enemy, how do you react?",
+  #   "choice1": "Study your opponent & their movements, concentrate on them.",
+  #   "choice2": "Quickly strategize how to use your environment around you.",
+  #   "choice3": "Welcome an all out brawl.",
+  #   "choice4": "Try to charm your enemy to lower tensions.",
+  #   "choice5": "Run away."
+  # },
+
+  Question.create(content: "Which type of music would you listen to first?", choice1: "Alternative", choice2: "Indie Rock", choice3: "Gangster Rap", choice4: "Punk Rock", choice5: "Jazz")
+
+
+  # {
+  #   "question": "Which type of music would you listen to first?",
+  #   "choice1": "Alternative",
+  #   "choice2": "Indie Rock",
+  #   "choice3": "Gangster Rap",
+  #   "choice4": "Punk Rock",
+  #   "choice5": "Jazz"
+  # },
+
+  Question.create(content: "Which American President is your Favorite?", choice1: "George Washington", choice2: "Thomas Jefferson", choice3: "Andrew Jackson", choice4: "John F. Kennedy", choice5: "FDR")
+
+
+  # {
+  #   "question": "Which American President is your Favorite?",
+  #   "choice1": "George Washington",
+  #   "choice2": "Thomas Jefferson",
+  #   "choice3": "Andrew Jackson",
+  #   "choice4": "John F. Kennedy",
+  #   "choice5": "FDR"
+  # },
+
+  Question.create(content: "Favorite Card Game", choice1: "Government", choice2: "Black Jack", choice3: "Golf", choice4: "99", choice5: "Rummy")
+
+
+  # {
+  #   "question": "Favorite Card Game",
+  #   "choice1": "Government",
+  #   "choice2": "Black Jack",
+  #   "choice3": "Golf",
+  #   "choice4": "99",
+  #   "choice5": "Rummy"
+  # }
+
+  Question.create(content: "What would you most likely get arrested for?", choice1: "Driving way over the speed limit", choice2: "Illegally downloading movies or music", choice3: "A bar fight", choice4: "Buying or Selling illegal drugs", choice5: "Stealing a car")
+
+
+]
