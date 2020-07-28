9bfa07b5ddeb321d10a0074a0feff162c6d6ee7c
Liam Tinney <ltinney@powerhrg.com>
2020-07-28 15:21:55 -0400

    update question.js

--- tmnt-quiz-frontend/question.js
+++ tmnt-quiz-frontend/question.js
@@ -21,89 +21,102 @@
 
 // }
 
-function Quiz(questions) {
-  this.score = 0;
-  this.questions = questions;
-  this.questionIndex = 0;
-}
-
-Quiz.prototype.getQuestionIndex = function() {
-  return this.questions[this.questionIndex];
-}
-
-Quiz.prototype.guess = function(answer) {
-  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-      this.score++;
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
   }
-  this.questionIndex++;
-}
 
-Quiz.prototype.isEnded = function() {
-  return this.questionIndex === this.questions.length;
-}
+  getQuestionsIndex() {
 
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
-}
-
-Question.prototype.isCorrectAnswer = function(choice) {
-  return this.answer === choice;
-}
-
-function populate() {
-  if(quiz.isEnded()) {
-      showScores();
   }
-  else {
-      // show question
-      var element = document.getElementById("question");
-      element.innerHTML = quiz.getQuestionIndex().text;
+}
 
-      // show options
-      var choices = quiz.getQuestionIndex().choices;
-      for(var i = 0; i < choices.length; i++) {
-          var element = document.getElementById("choice" + i);
-          element.innerHTML = choices[i];
-          // element.dataset.answer = choices["answer"]
-          guess("btn" + i, choices[i]);
-      }
-      showProgress();
-  }
-};
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("http://localhost:3000/questions")
+  .then(resp => resp.json())
+  .then(function(questions) {
+    populateQuiz(questions)
+  })
+})
 
-function guess(id, guess) {
-  var button = document.getElementById(id);
-  button.onclick = function() {
-      quiz.guess(guess);
-      populate();
-  }
-};
+function populateQuiz(questions) {
+  let quiz = new Quiz(questions)
+  quiz.getQuestionsIndex()
+  // does all the cool stuff
+}
 
-function showProgress() {
-  var currentQuestionNumber = quiz.questionIndex + 1;
-  var element = document.getElementById("progress");
-  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-};
+// Quiz.prototype.getQuestionIndex = function() {
+//   return this.questions[this.questionIndex];
+// }
 
-function showScores() {
-  var gameOverHTML = "<h1>Result</h1>";
-  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-  var element = document.getElementById("quiz");
-  element.innerHTML = gameOverHTML;
-};
+// Quiz.prototype.guess = function(answer) {
+//   if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+//       this.score++;
+//   }
+//   this.questionIndex++;
+// }
+
+// Quiz.prototype.isEnded = function() {
+//   return this.questionIndex === this.questions.length;
+// }
+
+// function Question(text, choices, answer) {
+//   this.text = text;
+//   this.choices = choices;
+//   this.answer = answer;
+// }
+
+// Question.prototype.isCorrectAnswer = function(choice) {
+//   return this.answer === choice;
+// }
+
+// function populate() {
+//   if(quiz.isEnded()) {
+//       showScores();
+//   }
+//   else {
+//       // show question
+//       var element = document.getElementById("question");
+//       element.innerHTML = quiz.getQuestionIndex().text;
+
+//       // show options
+//       var choices = quiz.getQuestionIndex().choices;
+//       for(var i = 0; i < choices.length; i++) {
+//           var element = document.getElementById("choice" + i);
+//           element.innerHTML = choices[i];
+//           // element.dataset.answer = choices["answer"]
+//           guess("btn" + i, choices[i]);
+//       }
+//       showProgress();
+//   }
+// };
+
+// function guess(id, guess) {
+//   var button = document.getElementById(id);
+//   button.onclick = function() {
+//       quiz.guess(guess);
+//       populate();
+//   }
+// };
+
+// function showProgress() {
+//   var currentQuestionNumber = quiz.questionIndex + 1;
+//   var element = document.getElementById("progress");
+//   element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+// };
+
+// function showScores() {
+//   var gameOverHTML = "<h1>Result</h1>";
+//   gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+//   var element = document.getElementById("quiz");
+//   element.innerHTML = gameOverHTML;
+// };
 
 // create questions here
 // fetch(/questions) to fill out this array
 // this endpoint will also need choices for each question
-
-function fetchQuestions() {
-  fetch("http://localhost:3000/questions")
-  .then(resp => resp.json())
-  .then(function(questions) {
-  })
-}
 
 
 // var questions = [
@@ -125,10 +138,10 @@ function fetchQuestions() {
 // ];
 
 // create quiz
-var quiz = new Quiz(fetchQuestions());
+// var quiz = new Quiz(fetchQuestions());
 
 // display quiz
-populate();
+// populate();
 
 
 // for every question each choice has a hidden data value with a number.
