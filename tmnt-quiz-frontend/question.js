3b27185e10fd51e24ec2bdcc970b4192c782b416
Liam Tinney <ltinney@powerhrg.com>
2020-07-29 14:08:36 -0400

    update js

--- tmnt-quiz-frontend/question.js
+++ tmnt-quiz-frontend/question.js
@@ -21,8 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
         // displays score in the DOM
         const showScore = document.getElementById("score")
         showScore.innerHTML = "Your score was: " + score
-        // console.log("Your score was: " + score)
-      }else{
+
+        fetch(`http://localhost:3000/questions/final_image?total_correct=${score}`)
+          .then(resp => resp.json())
+          .then(imageUrl => {
+             console.log(imageUrl)
+          })
+
+      } else {
         populateQuiz()
       }
     })
@@ -51,81 +57,5 @@ function populateQuiz() {
     const progQuestion = currentQuestionNumber + 1
     const progElement = document.getElementById("progress");
     progElement.innerHTML = "Question " + progQuestion + " of " + questions.length
+
 }
-
-
-// function Quiz(questions) {
-//   this.score = 0;
-//   this.questions = questions;
-//   this.questionIndex = 0;
-// }
-
-// Quiz.prototype.getQuestionIndex = function() {
-//   return this.questions[this.questionIndex];
-// }
-
-// Quiz.prototype.guess = function(answer) {
-//   if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-//       this.score++;
-//   }
-
-//   this.questionIndex++;
-// }
-
-// Quiz.prototype.isEnded = function() {
-//   return this.questionIndex === this.questions.length;
-// }
-
-// function Question(text, choices, answer) {
-//   this.text = text;
-//   this.choices = choices;
-//   this.answer = answer;
-// }
-
-// Question.prototype.isCorrectAnswer = function(choice) {
-//   return this.answer === choice;
-// }
-
-
-// function populate() {
-//   if(quiz.isEnded()) {
-//       showScores();
-//   }
-//   else {
-//       // show question
-//       var element = document.getElementById("question");
-//       element.innerHTML = quiz.getQuestionIndex().text;
-
-//       // show options
-//       var choices = quiz.getQuestionIndex().choices;
-//       for(var i = 0; i < choices.length; i++) {
-//           var element = document.getElementById("choice" + i);
-//           element.innerHTML = choices[i];
-//           guess("btn" + i, choices[i]);
-//       }
-
-//       showProgress();
-//   }
-// };
-
-// function guess(id, guess) {
-//   var button = document.getElementById(id);
-//   button.onclick = function() {
-//       quiz.guess(guess);
-//       populate();
-//   }
-// };
-
-
-// function showProgress() {
-//   var currentQuestionNumber = quiz.questionIndex + 1;
-//   var element = document.getElementById("progress");
-//   element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-// };
-
-// function showScores() {
-//   var gameOverHTML = "<h1>Result</h1>";
-//   gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-//   var element = document.getElementById("quiz");
-//   element.innerHTML = gameOverHTML;
-// };
--- which-tmnt-are-you-quiz-app/app/controllers/questions_controller.rb
+++ which-tmnt-are-you-quiz-app/app/controllers/questions_controller.rb
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
   end
 
   def final_image
-
+    :total_correct.to_i
 
 
   end
