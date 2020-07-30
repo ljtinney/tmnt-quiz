bc7c33de8438cc8ac191415d74eddbc010a6e6b2
Liam Tinney <ltinney@powerhrg.com>
2020-07-30 09:38:45 -0400

    refactor Question Model & Controller

--- which-tmnt-are-you-quiz-app/app/controllers/questions_controller.rb
+++ which-tmnt-are-you-quiz-app/app/controllers/questions_controller.rb
@@ -13,23 +13,8 @@ class QuestionsController < ApplicationController
 
   def final_image
     score = params[:total_correct].to_i
-
-    if score < 10
-      imageUrl = "https://i.pinimg.com/474x/c0/08/3e/c0083ecbd47fc5eeadd14d881e2a1f0d--funny-captions-teenage-mutant-ninja-turtles.jpg"
-      render json: imageUrl.to_json()
-
-    elsif score.between?(9,16)
-      imageUrl = "https://i.pinimg.com/474x/c0/08/3e/c0083ecbd47fc5eeadd14d881e2a1f0d--funny-captions-teenage-mutant-ninja-turtles.jpg"
-      render json: imageUrl.to_json()
-
-    elsif score.between?(15,20)
-      imageUrl = "https://static2.srcdn.com/wordpress/wp-content/uploads/2017/03/Pizza-Time-Teenage-Mutant-Ninja-Turtles.jpg"
-      render json: imageUrl.to_json()
-
-    else score > 19
-      imageUrl = "https://www.syfy.com/sites/syfy/files/styles/1200x680_hero/public/2020/04/teenage-mutant-ninja-turtles-1990.jpg"
-      render json: imageUrl.to_json()
-    end
+    image_url = Question.image_url(score)
+    render json: image_url.to_json
   end
 
 end
--- which-tmnt-are-you-quiz-app/app/models/question.rb
+++ which-tmnt-are-you-quiz-app/app/models/question.rb
@@ -1,3 +1,19 @@
 class Question < ApplicationRecord
   has_many :choices
+
+  def self.image_url(score)
+    if score < 10
+      "https://i.pinimg.com/474x/c0/08/3e/c0083ecbd47fc5eeadd14d881e2a1f0d--funny-captions-teenage-mutant-ninja-turtles.jpg"
+
+    elsif score.between?(9,15)
+      "https://29.media.tumblr.com/tumblr_m24zmuw1SO1rojksqo1_500.jpg"
+
+    elsif score.between?(14,20)
+      "https://static2.srcdn.com/wordpress/wp-content/uploads/2017/03/Pizza-Time-Teenage-Mutant-Ninja-Turtles.jpg"
+
+    else
+      "https://www.syfy.com/sites/syfy/files/styles/1200x680_hero/public/2020/04/teenage-mutant-ninja-turtles-1990.jpg"
+    end
+  end
+
 end
